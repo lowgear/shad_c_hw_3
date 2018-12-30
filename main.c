ae121888bdad6d5f4edc63eac5c01282e3729414
aidar.islamov <aidar.islamov@5CD8242FCP>
2018-12-31 02:38:02 +0500

    calls and invocation done

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <inttypes.h>
+#include <stdlib.h>
 
 #include "utils/vector.h"
 
@@ -9,6 +10,7 @@ enum OpRetCode {
     Ok,
     ArgNumberMismatch,
     ArgTypeMismatch,
+    AllocationFailure,
     DBZ
 };
 
@@ -29,7 +31,7 @@ DEF_VECTOR(CallArgV, struct Expression *);
 
 struct Expression {
     union {
-        struct CallArgV *callArgV;
+        struct CallArgV *paramsV;
         struct Object *object;
         size_t varId;
     };
@@ -61,38 +63,59 @@ struct LazyExpr {
     struct Object *value;
 };
 
-enum OpRetCode EvalExpr(struct Expression *expression, struct ArgV *argv, struct Object *out);
+enum OpRetCode EvalExpr(struct Expression *expression, struct ArgV *argv, struct Object **out);
 
-enum OpRetCode GetLazyExprVal(struct LazyExpr *lazyExpr, struct Object *out) {
+enum OpRetCode GetLazyExprVal(struct LazyExpr *lazyExpr, struct Object **out) {
     if (lazyExpr->value == NULL) {
-        enum OpRetCode rc = EvalExpr(lazyExpr->expression, lazyExpr->argv, lazyExpr->value);
+        enum OpRetCode rc = EvalExpr(lazyExpr->expression, lazyExpr->argv, &lazyExpr->value);
         if (rc != Ok)
             return rc;
     }
-    out = lazyExpr->value;
+    *out = lazyExpr->value;
     return Ok;
 }
 
-DEF_VECTOR(ArgV, struct LazyExpr*);
+DEF_VECTOR(ArgV, struct LazyExpr);
 
-enum OpRetCode Invoke(struct Function *function, struct ArgV *argv, struct Object *out) {
+enum OpRetCode EvalCall(struct CallArgV *callArgV, struct ArgV *argv, struct Object **out) {
+    struct Object *func;
 
+    enum OpRetCode rc = EvalExpr(callArgV->array[0], argv, &func);
+    if (rc != Ok)
+        return rc;
+    if (func->type != Func)
+        return ArgTypeMismatch;
+    size_t paramsNum = callArgV->size;
+    if (paramsNum - 1 != func->func->argc)
+        return ArgNumberMismatch;
+    struct ArgV *innerArgV = NewArgV(callArgV->size - 1);
+    if (innerArgV == NULL)
+        return AllocationFailure;
+    for (size_t i = 0; i < paramsNum; ++i) {
+        struct Expression *const curExpr = callArgV->array[i + 1];
+        innerArgV->array[i] =
+                curExpr->expType == Var
+                ? argv->array[curExpr->varId]
+                : (struct LazyExpr) {
+                        .expression = curExpr,
+                        .argv = argv,
+                        .value = NULL
+                };
+    }
+    rc = EvalExpr(func->func->expression, innerArgV, out);
+    free(innerArgV);
+    return rc;
 }
 
-enum OpRetCode EvalExpr(struct Expression *expression, struct ArgV *argv, struct Object *out) {
-    struct Object *func;
-    enum OpRetCode rc;
+enum OpRetCode EvalExpr(struct Expression *expression, struct ArgV *argv, struct Object **out) {
     switch (expression->expType) {
         case Call:
-            rc = EvalExpr(expression->callArgV->array[0], argv, func);
-            if (rc != Ok)
-                return rc;
-
+            return EvalCall(expression->paramsV, argv, out);
         case Const:
-            out = expression->object;
+            *out = expression->object;
             return Ok;
         case Var:
-            return GetLazyExprVal(argv->array[expression->varId], out);
+            return GetLazyExprVal(&argv->array[expression->varId], out);
     }
 }
 
--- utils/vector.h
+++ utils/vector.h
@@ -1,5 +1,17 @@
+#pragma once
+
+#include <stdlib.h>
+
 #define DEF_VECTOR(name, T) \
 struct name { \
     size_t size; \
     T array[1]; \
+}; \
+\
+struct name* New##name(size_t size) { \
+    struct name* v = malloc(sizeof(struct name) + sizeof(T) * size - sizeof(T)); \
+    if (v == NULL) \
+        return NULL; \
+    v->size = size; \
+    return v; \
 }
